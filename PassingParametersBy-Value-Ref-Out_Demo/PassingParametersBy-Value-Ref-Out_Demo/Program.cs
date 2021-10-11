39390f079df71e049566d24933c2ae22ce8d2791
jdw <johndward01@gmail.com>
2021-10-11 00:15:53 -0500

    added regions

--- PassingParametersBy-Value-Ref-Out_Demo/PassingParametersBy-Value-Ref-Out_Demo/Program.cs
+++ PassingParametersBy-Value-Ref-Out_Demo/PassingParametersBy-Value-Ref-Out_Demo/Program.cs
@@ -6,23 +6,25 @@ namespace PassingParametersBy_Value_Ref_Out_Demo
     {
         static void Main(string[] args)
         {
+            #region Summary
+
             /*
                 There are three ways to pass arguments to a method when the method is called: By value, By reference, and as Output.
                 By value copies the argument's value into the method's formal parameter. 
                 Here, we can make changes to the parameter within the method without having any effect on the argument. 
             */
 
+            #endregion
 
 
             #region Ref Parameters
-                   
-                //Ref is a keyword in C# which is used for passing the value types by reference. 
-                //Or we can say that if any changes made in this argument in the method will reflect in that variable when the control return to the calling method.
-                //The ref parameter does not pass the property. 
-                //In ref parameters, it is necessary that the parameters should initialize before it pass to ref. 
-                //The passing of value through the ref parameter is useful when the called method also needs to change the value of the passed parameter. 
-                    
-            #endregion
+
+            //Ref is a keyword in C# which is used for passing the value types by reference. 
+            //Or we can say that if any changes made in this argument in the method will reflect in that variable when the control return to the calling method.
+            //The ref parameter does not pass the property. 
+            //In ref parameters, it is necessary that the parameters should initialize before it pass to ref. 
+            //The passing of value through the ref parameter is useful when the called method also needs to change the value of the passed parameter. 
+                        
 
             // Assigning value 
             string val = "Dog";
@@ -33,16 +35,18 @@ namespace PassingParametersBy_Value_Ref_Out_Demo
             // Display the given value 
             Console.WriteLine(val);
 
+            #endregion
+
 
             #region Out Parameters
-                
-                //The out is a keyword in C# which is used for the passing the arguments to methods as a reference type. 
-                //It is generally used when a method returns multiple values. 
-                //The out parameter does not pass the property. 
-                //It is not necessary to initialize parameters before it passes to out.
-                //The declaring of parameter throughout parameter is useful when a method returns multiple values.
+
+            //The out is a keyword in C# which is used for the passing the arguments to methods as a reference type. 
+            //It is generally used when a method returns multiple values. 
+            //The out parameter does not pass the property. 
+            //It is not necessary to initialize parameters before it passes to out.
+            //The declaring of parameter throughout parameter is useful when a method returns multiple values.
+
             
-            #endregion
 
             // Creating variable 
             // without assigning value 
@@ -56,10 +60,11 @@ namespace PassingParametersBy_Value_Ref_Out_Demo
             Console.WriteLine("The sum of"
               + " the value is: {0}", num);
 
+            #endregion
         }
 
         #region Methods
-        
+
         static void CompareValue(ref string val1)
         {
             // Compare the value 
